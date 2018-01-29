fe8118e28ab4a00e6d82593c34fbb6a1b2b38974
Cristian Danilo Ramirez Vargas <rvcristiand@unal.edu.co>
2018-01-28 21:15:08 -0500

    Add files via upload

---
+++ cgi101.py
@@ -0,0 +1,12 @@
+import cgi
+
+form = cgi.FieldStorage()
+print('Content-type: text/html\n')
+print('<title>Reply Page</title>')
+if not 'user' in form:
+    print('<h1>Who are you?</h1>')
+else:
+    print('<h1>Hello <i>{}</i>!</h1>'.format(cgi.escape(form['user'].value)))
+
+
+
